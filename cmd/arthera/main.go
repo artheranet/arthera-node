548656e8cdc332b48c6ae699f7c0e4aa78864442
flaviusburca <flaviusburca@users.noreply.github.com>
2023-05-07 09:48:45 +0300

    forbid building with go1.20 or newer

--- cmd/arthera/main.go
+++ cmd/arthera/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/artheranet/arthera-node/cmd/arthera/launcher"
 )
 
 func main() {
+	// TODO erase after compatibility issues with go1.20 are fixed
+	var majorVer int
+	var minorVer int
+	var other string
+	n, err := fmt.Sscanf(runtime.Version(), "go%d.%d%s", &majorVer, &minorVer, &other)
+	if n >= 2 && err == nil {
+		if (majorVer*100 + minorVer) > 119 {
+			panic(runtime.Version() + " is not supported, please downgrade your go compiler to go 1.19 or older")
+		}
+	}
 	fmt.Println("▄▀█ █▀█ ▀█▀ █░█ █▀▀ █▀█ ▄▀█\n█▀█ █▀▄ ░█░ █▀█ ██▄ █▀▄ █▀█")
 	if err := launcher.Launch(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)

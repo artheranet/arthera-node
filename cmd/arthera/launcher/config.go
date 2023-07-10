ac01f9eef35030903e09ea87a6469d2ac167f269
DevOne dev@arthera.net <dev@arthera.net>
2023-07-10 23:17:51 +0300

    Fix searching for indexed events by name

--- cmd/arthera/launcher/config.go
+++ cmd/arthera/launcher/config.go
@@ -401,7 +401,7 @@ func cacheScaler(ctx *cli.Context) cachescale.Func {
 	if !ctx.GlobalIsSet(CacheFlag.Name) {
 		recommendedCache := totalMemory / 2
 		if recommendedCache > baseSize {
-			log.Warn(fmt.Sprintf("Please add '--%s %d' flag to allocate more cache for Opera. Total memory is %d MB.", CacheFlag.Name, recommendedCache, totalMemory))
+			log.Warn(fmt.Sprintf("Please add '--%s %d' flag to allocate more cache for Arthera. Total memory is %d MB.", CacheFlag.Name, recommendedCache, totalMemory))
 		}
 		return cachescale.Identity
 	}

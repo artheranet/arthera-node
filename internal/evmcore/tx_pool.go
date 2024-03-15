3aa198945ef2bc3e587a95cf37dca4708a278427
flaviusburca <flaviusburca@users.noreply.github.com>
2024-03-15 08:34:44 +0000

    Revert "Fix price filtering in tx pool to prevent low price legacy transaction from spamming the pool (#23855)"
    This reverts commit 393dbfc1004a9020def8a0013783c5024fddfd5c.

--- internal/evmcore/tx_pool.go
+++ internal/evmcore/tx_pool.go
@@ -639,10 +639,9 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	if err != nil {
 		return ErrInvalidSender
 	}
+	// Drop non-local transactions under our own minimal accepted gas price or tip
 	local = local || pool.locals.contains(from) // account may be local even if the transaction arrived from the network
-	// Drop non-local transactions under our own minimal accepted gas price or tip.
-	pendingBaseFee := pool.priced.urgent.baseFee
-	if !local && tx.EffectiveGasTipIntCmp(pool.gasPrice, pendingBaseFee) < 0 {
+	if !local && tx.GasTipCapIntCmp(pool.gasPrice) < 0 {
 		return ErrUnderpriced
 	}
 	// Ensure Arthera-specific hard bounds
